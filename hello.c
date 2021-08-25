1dafa3b4ec62ab62f1b715223c0528fffd3e8313
noahzot <unverzagt.noah@huskers.unl.edu>
2021-08-25 23:28:43 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Noah Unverzagt
+ * Date: 2021
+ * 
+ * a simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Hello world!\n");
+     
+     return 0;
+ }
