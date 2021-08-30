4aae5bdd4000181d048f08d48511be16b08d3603
noahzot <unverzagt.noah@huskers.unl.edu>
2021-08-30 20:44:01 +0000

    added my major

--- hello.c
+++ hello.c
@@ -1,16 +1,16 @@
 /**
  * Author: Noah Unverzagt
  * Date: 2021
- * 
+ *
  * a simple hello world program
  */
  #include <stdlib.h>
  #include <stdio.h>
- 
+
  int main(int argc, char **argv) {
-     
-     printf("Noah Unverzagt\n");
-     printf("Broadcasting\n");
-     
+
+     printf("Hello World, I'm Noah Unverzagt\n");
+     printf("My major is Broadcasting\n");
+
      return 0;
  }
